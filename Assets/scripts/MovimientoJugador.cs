b7740f5d107cef7db252fe7bb9c224fff01e1d0a
Natsan12 <153123678+Natsan12@users.noreply.github.com>
2024-02-09 22:53:54 -0500

    09/02/2024
    Modificación del script

--- Assets/scripts/MovimientoJugador.cs
+++ Assets/scripts/MovimientoJugador.cs
@@ -7,16 +7,21 @@ public class NewBehaviourScript : MonoBehaviour
 	public float velocidadCaminar = 5f;
 	public float velocidadSalto = 10f;
 	public float alturaAgacharse = 0.5f;
-	public string Horizontal, Vertical;
+	
 
 	private bool estaAgachado=false;
+	private Rigidbody rb;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
-		float movimientoHorizontal = Input.GetAxisRaw(Horizontal);
-		float movimientoVertical = Input.GetAxisRaw(Vertical);
+		float movimientoHorizontal = Input.GetAxisRaw("Horizontal");
+		float movimientoVertical = Input.GetAxisRaw("Vertical");
 
-		transform.position = new Vector2(movimientoVertical, movimientoHorizontal * velocidadCaminar * Time.deltaTime);
+		rb.velocity = new Vector2(movimientoHorizontal,rb.velocity.y);
 	}
 
 
