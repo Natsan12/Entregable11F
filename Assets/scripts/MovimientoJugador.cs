b754997b5713c9018c52b89ed85727eda18816d8
Natsan12 <153123678+Natsan12@users.noreply.github.com>
2024-02-13 22:24:55 -0500

    13/02/2024
    Se agregan tres animaciones, pero se deben corregir porque cunado las aplico no permiten que el jugador se mueva.

--- Assets/scripts/MovimientoJugador.cs
+++ Assets/scripts/MovimientoJugador.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -7,18 +8,29 @@ public class NewBehaviourScript : MonoBehaviour
 	public float velocidadCaminar = 5f;
 	public float fuerzaSalto = 10f;
     public Rigidbody2D rb;
-	
-
+    private bool Grounded;
+    //private Animator animator;
 	
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+       // animator.GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
 		float movimientoHorizontal = Input.GetAxisRaw("Horizontal");
-		
+       // animator.SetBool("Jump", movimientoHorizontal!= 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down*0.1f, Color.red); //Dibujar el ray
+
+
+        if (Physics2D.Raycast(transform.position, Vector2.down, 0.1f))
+        {
+            Grounded = true;            
+        }
+        else Grounded = false;
+
 
 		rb.velocity = new Vector2(movimientoHorizontal*velocidadCaminar,rb.velocity.y);
         Saltar();
@@ -26,7 +38,7 @@ public class NewBehaviourScript : MonoBehaviour
     private void Saltar()
     {
         float movimientoVertical = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(KeyCode.UpArrow)) 
+        if (Input.GetKey(KeyCode.UpArrow) && Grounded) 
         {
             rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
         }
