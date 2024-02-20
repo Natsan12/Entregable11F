18a5927becdd1beceac1e3af96f54834443f8f43
Natsan12 <153123678+Natsan12@users.noreply.github.com>
2024-02-20 17:34:44 -0500

    20/02/2024
    Ubicación de cámaras y  animaciones

--- Assets/scripts/CamaraPlayer.cs
+++ Assets/scripts/CamaraPlayer.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CamaraPlayer : MonoBehaviour
 {
-    public GameObject AstroStay;
+    public GameObject puntoVisual;
+
 
 
     // Update is called once per frame
     void Update()
     {
         Vector3 position= transform.position;
-        position.x = AstroStay.transform.position.x;
+        position.x = puntoVisual.transform.position.x;
         transform.position = position;
+
+       
     }
 }
